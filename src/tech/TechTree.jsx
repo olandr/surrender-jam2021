ce4e994826414af8758a2fb96f99b641303b51df
olandr <sonomis1@gmail.com>
2021-05-14 21:40:35 +0200

    Adds correct tech tree

--- src/tech/TechTree.jsx
+++ src/tech/TechTree.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Graph } from "react-d3-graph";
+import { Graph } from 'react-d3-graph';
 import { generateTree } from '../utils/tech_tree.js';
 
 export const TechTree = () => {
@@ -8,14 +8,20 @@ export const TechTree = () => {
     // the graph configuration, just override the ones you need
     const myConfig = {
         nodeHighlightBehavior: true,
+        width: 1900,
+        height: 950,
+        directed: true,
         node: {
-        color: "lightgreen",
-        size: 120,
-        highlightStrokeColor: "blue",
+            color: '#FF5733',
+            size: 200,
+            highlightStrokeColor: '#FD2D00',
+            labelProperty: 'name'
         },
         link: {
-        highlightColor: "lightblue",
+            color:'#FFB312',
+            highlightColor: '#FFBE33',
         },
+
     };
     
     const onClickNode = function(nodeId) {
@@ -25,18 +31,24 @@ export const TechTree = () => {
     const onClickLink = function(source, target) {
         window.alert(`Clicked link between ${source} and ${target}`);
     };
-  
+
+    const onToolTip = (nodeID, node) => {
+        console.log(node['_indesc']);
+    }
+
     useEffect(() => {
         setTree(generateTree());
+
     }, []);
     
     return (
         tree && <Graph
-            id="graph-id" // id is mandatory
+            id='graph-id' // id is mandatory
             data={tree}
             config={myConfig}
             onClickNode={onClickNode}
             onClickLink={onClickLink}
+            onMouseOverNode={onToolTip}
         />
     )
 }
