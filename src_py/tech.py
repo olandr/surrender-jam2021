dde6497f150c55c4e566aa4980d81e3e88cb13d5
olandr <sonomis1@gmail.com>
2021-05-14 23:21:34 +0200

    Updates techs to take a rule func

--- src_py/tech.py
+++ src_py/tech.py
@@ -14,13 +14,17 @@ with open(datadir + inpath) as f:
         tmp['id'] = int(term[0])
         tmp['name'] = term[1]
         tmp['_indesc'] = term[2]
+        if len(term) > 4:
+            tmp['hidden_pg'] = term[4]
         nodes.append(tmp)
         
         tmp = {}
-        if term[3] != '-':
+        if len(term) > 3:
             tmp['source'] = int(term[3])
             tmp['target'] = int(term[0])
             links.append(tmp)
+        
+
     json_dump['nodes'] = nodes
     json_dump['links'] = links
     print(json_dump)
