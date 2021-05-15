22ff72079e13c4d55feff318162f197d60c7d9cb
olandr <sonomis1@gmail.com>
2021-05-15 16:25:45 +0200

    Adds costs techs

--- src_py/tech.py
+++ src_py/tech.py
@@ -6,6 +6,7 @@ outpath = 'techs.json'
 
 json_dump = {}
 with open(datadir + inpath) as f:
+    header = ['Name','Internal',' Desc', 'Parent','cost','hidden_pg','present_pg']
     nodes = []
     links = []
     for line in f:
@@ -13,16 +14,23 @@ with open(datadir + inpath) as f:
         term = line.strip().split('\t')
         tmp['id'] = int(term[0])
         tmp['label'] = term[1]
-        tmp['title'] = term[2]
-        if len(term) > 4:
-            tmp['hidden_pg'] = term[4]
+        tmp['title'] = term[2] + ' (' + term[4] + 'p)'
+        
+        rel = {}
+        if term[3] != '-':
+            rel['from'] = int(term[3])
+            rel['to'] = int(term[0])
+            links.append(rel)
+        
+        tmp['cost'] = int(term[4])
+
+        try:
+            if term[5] != '' :
+                tmp['hidden_pg'] = term[5]
+        except:
+            pass
         nodes.append(tmp)
         
-        tmp = {}
-        if len(term) > 3:
-            tmp['from'] = int(term[3])
-            tmp['to'] = int(term[0])
-            links.append(tmp)
         
 
     json_dump['nodes'] = nodes
