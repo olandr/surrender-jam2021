fadb665f9d41b813a8a8d785b01785c1aa697011
olandr <sonomis1@gmail.com>
2021-05-15 14:55:04 +0200

    Fixes capitalisation bloddy hell (about 3h to find that one)

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,16 +3,16 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Bar } from "../bar/Bar.jsx";
 
-import { TechTree } from "../tech/Techtree.jsx";
+import { TechTree } from "../tech/TechTree.jsx";
 import { GameLoop } from "../game/GameLoop.jsx";
 import { loadRules } from "../rules/Rules.jsx";
 
 export const App = () => {
   const [rules, setRules] = useState([]);
-  const [hash, setHash] = useState("game");
+  const [hash, setHash] = useState("tree");
   const [points, setPoints] = useState(0);
   // FIXME: get techs from the actual tech tree
-  const [techTree, setTechTree] = useState([0, 8, 11, 15]);
+  const [techTree, setTechTree] = useState([0]);
 
   useEffect(() => {
     setRules(loadRules(techTree, points, setPoints));
