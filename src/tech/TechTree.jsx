20442aaaa2015a237e02c0910af70cccb11364f2
olandr <sonomis1@gmail.com>
2021-05-15 15:47:01 +0200

    Adds styling to techtree

--- src/tech/TechTree.jsx
+++ src/tech/TechTree.jsx
@@ -4,36 +4,51 @@ import Graph from "react-graph-vis";
 
 export const TechTree = () => {
   const [tree, setTree] = useState(null);
-  // the graph configuration, just override the ones you need
-  const options = {
-    nodes: {
-      color: "#FF5733",
-      size: 200,
-      //highlightStrokeColor: "#FD2D00",
-      label: "label",
-    },
-    edges: {
-      color: "#FFB312",
-    },
-  };
-  const events = {
-    select: (event) => {
-      let { nodes, edges } = event;
-      window.alert(`Clicked node ${nodes[0]}`);
-      nodes[0] && console.log(nodes[0]["title"]);
-    },
-  };
 
   useEffect(() => {
     setTree(generateTree());
   }, []);
 
+  // the graph configuration, just override the ones you need
+  const options = {
+    interaction: {
+      multiselect: true,
+      hover: true,
+    },
+    nodes: {
+      size: 200,
+      color: {
+        background: "#FF5733",
+        border: "#2B42FF",
+        highlight: {
+          background: "#FFB312",
+        },
+        hover: {
+          background: "#FF0000",
+        },
+      },
+      label: "label",
+      font: {
+        face: '"Reenie Beanie", cursive',
+        size: 20,
+      },
+    },
+    edges: {
+      color: "#FFB312",
+    },
+  };
+
+  const handleNetwork = (network) => {
+    network.selectNodes([0, 1, 2]);
+  };
+
   return (
     tree && (
       <Graph
-        id="graph-id" // id is mandatory
+        id="tech-graph"
         graph={tree}
         options={options}
+        getNetwork={(net) => handleNetwork(net)}
       />
     )
   );
